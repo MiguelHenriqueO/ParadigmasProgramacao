5cf34fd179c6fcd495ef306c6cdc9f793ba31c1d
MiguelHenriqueO <miguelhenriqueisaac@gmail.com>
2026-02-26 20:37:26 -0300

    aula 26/02

--- src/Carro.java
+++ src/Carro.java
@@ -14,6 +14,41 @@ public class Carro{
         this.motor = motor;
     }
 
+    //ligar carro
+    public void ligarCarro(){
+        if (!this.motor){
+            this.motor = true;
+        }
+        System.out.println("motor ligado");
+    }
+
+    public void acelerarCarro(float x){
+       if (this.motor) {
+           this.velocidade += x;
+           System.out.println("nova velocidade: " + this.velocidade);
+       }
+    }
+
+    public void frearCarro(float x){
+        if (!this.motor && this.velocidade - x > 0){
+            this.velocidade -= x;
+            System.out.println("Nova velocidade: " + this.velocidade);
+        }
+        else{
+            System.out.println("Não freou");
+        }
+
+    }
+
+    //desligar carro
+    public void desligarCarro(){
+        if(this.motor){
+            this.motor = false;
+            this.velocidade = 0;
+        }
+        System.out.println("motor desligado");
+    }
+
     public void mostrarModelo(){
         String info = String.format("Marca: %s, \n Modelo: %s, \n Velocidade: %.2f, \n Motor ligado: %b \n", marca, modelo, velocidade, motor);
         JOptionPane.showMessageDialog(null, info, "Informações do carro", JOptionPane.INFORMATION_MESSAGE);
@@ -21,7 +56,7 @@ public class Carro{
 
     //metodo que converte o objeto em String
     // this. representa o objeto que chama o metodo
-    /*public String toString(){
+    public String toString(){
         return "Marca: " + this.marca + " Modelo: " + this.modelo + " Velocidade: " + this.velocidade + " Motor: " + this.motor;
-    }*/
+    }
 }
--- src/TestaPessoa.java
+++
@@ -1,8 +0,0 @@
-public class TestaPessoa {
-    public static void main(String[] args){
-        Pessoa pessoa = new Pessoa("miguel", 20, 1.70f);
-
-
-        pessoa.imprimirDados();
-    }
-}
